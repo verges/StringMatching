9b858a328f7f99243bac4b6ba0cbe4becc35bc4b
Rafał Tulwin <rtulwin@gmail.com>
2011-06-20 01:56:28 +0200

    test

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 #include <AboutDialog.h>
 
 int main(int argc, char *argv[]) {
-    // initialize resources, if needed
-    // Q_INIT_RESOURCE(resfile);
+    //initialize resources, if needed
+    //Q_INIT_RESOURCE(resfile);
 
     QApplication app(argc, argv);
     MainWindow mainWindow;
